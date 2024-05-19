78e73526a35f70b9d450f0abb9786c4226ddfc05
Coboy16 <smautrap.16@gmail.com>
2024-05-19 02:11:12 -0500

    ready database

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import db from "./config/mongo.config"
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
+db().then(() => console.log('Ready Data Base'));
+app.listen(PORT, () => console.log(`Ready port: ${PORT}`));
 
